0da340efb37f6367b98ceb79338237939ae3cfaa
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-13 20:24:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='pytorch-supporter',
-	version='0.0.14',
+	version='0.0.15',
 	description='Pytorch supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
